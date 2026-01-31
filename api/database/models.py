9b8d0cd9d9aad7255d80d9ae135f315a6a2563a3
nikoethonai <niko.strongioglou@ethon.ai>
2026-01-31 22:44:34 +0100

    e2e flow I

--- api/database/models.py
+++ api/database/models.py
@@ -5,8 +5,14 @@ Base = declarative_base()
 
 
 class User(Base):
+    """User model."""
     __tablename__ = "users"
 
     api_key = Column(String(36), primary_key=True)
+    """API key."""
+
     name = Column(String(255), nullable=False)
+    """Name."""
+
     credits = Column(Integer, nullable=False)
+    """Credits."""
