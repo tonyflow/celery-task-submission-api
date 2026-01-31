eaf55036ed00f0540b9ff0323c6b94bcc635f0c1
nikoethonai <niko.strongioglou@ethon.ai>
2026-01-31 22:44:29 +0100

    e2e flow

--- api/api.py
+++ api/api.py
@@ -1,41 +1,18 @@
-from contextlib import asynccontextmanager
-
-import asyncpg
 import redis
 from celery import Celery
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, HTTPException
+from fastapi.params import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from sqlalchemy import select
 
 from database.engine import async_session
 from database.models import User
-from models import UserResponse
+from models import UserResponse, TaskResponseBase, TaskResponseState
 
 cache = redis.Redis(host="redis", port=6379)
 celery_app = Celery("worker", broker="amqp://guest:guest@rabbitmq:5672//", backend="redis://redis:6379/0")
 
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    print("Starting up")
-    app.state.db_pool = await asyncpg.create_pool(
-        user="postgres",
-        password="postgres",
-        database="postgres",
-        host="postgres",
-        port=5432,
-        min_size=1,
-        max_size=10,
-    )
-
-    yield
-
-    await app.state.db_pool.close()
-    print("Shutdown complete")
-
-
-app = FastAPI(
-    lifespan=lifespan,
-)
+app = FastAPI()
 
 
 def get_hit_count():
@@ -48,23 +25,42 @@ def hello():
     return "Hello World! I have been seen {} times.".format(count)
 
 
-@app.get("/users",response_model=list[UserResponse])
+@app.get("/users", response_model=list[UserResponse])
 async def users():
+    """List all users."""
     async with async_session() as session:
         result = await session.execute(select(User))
         return result.scalars().all()
 
 
-@app.post("/task")
-def create_task(x: int = Query(...), y: int = Query(...)):
+security = HTTPBearer()
+
+
+async def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)) -> User:
+    """Get current user from token."""
+    token = credentials.credentials
+    async with async_session() as session:
+        result = await session.execute(select(User).where(User.api_key == token))
+        user = result.scalar_one_or_none()
+
+        if not user:
+            raise HTTPException(status_code=401, detail="Unauthorized")
+
+        return user
+
+
+@app.post("/task", response_model=TaskResponseBase)
+def create_task(x: int = Query(...), y: int = Query(...), user: User = Depends(get_current_user)):
+    """Create a number addition task."""
     task = celery_app.send_task("worker.add", args=[x, y])
-    return {"task_id": task.id}
+    return TaskResponseBase(task_id=task.id)
 
 
-@app.get("/poll")
+@app.get("/poll", response_model=TaskResponseState)
 def poll_task(task_id: str = Query(...)):
+    """Poll task state."""
     result = celery_app.AsyncResult(task_id)
-    response = {"task_id": task_id, "state": result.state}
+    response = TaskResponseState(task_id=task_id, state=result.state)
     if result.ready():
-        response["result"] = result.result
+        response.result = result.result
     return response
--- api/models.py
+++ api/models.py
@@ -1,4 +1,25 @@
 from pydantic import BaseModel
 
+
 class UserResponse(BaseModel):
-    name:str
+    """User response model."""
+
+    name: str
+    """User name"""
+
+
+class TaskResponseBase(BaseModel):
+    """Task response base model."""
+
+    task_id: str
+    """Task id."""
+
+
+class TaskResponseState(TaskResponseBase):
+    """Task response state and result."""
+
+    state: str
+    """Task state."""
+
+    result: int | None = None
+    """Task result."""
